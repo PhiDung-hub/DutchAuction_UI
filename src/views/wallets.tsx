15211470a904f7a9f38848fac37b8a99226ba603
Phidung-hub <dungho01092002@gmail.com>
2023-10-29 01:23:03 +0800

    update wallet info

--- src/views/wallets.tsx
+++ src/views/wallets.tsx
@@ -1,6 +1,8 @@
 "use client"
 
-import { ReactElement } from 'react'
+import classNames from 'classnames'
+import { ReactElement, useEffect, useState } from 'react'
+import { twMerge } from 'tailwind-merge'
 import {
   useAccount,
   useConnect,
@@ -13,6 +15,8 @@ import CoinbaseWalletIcon from '~/icons/wallets/CoinbaseWallet'
 import MetaMaskIcon from '~/icons/wallets/MetaMask'
 import WalletConnectIcon from '~/icons/wallets/WalletConnect'
 import { truncateAddress } from '~/lib/blockchain'
+import { getCurrentBlock } from '~/lib/client'
+import { TOKEN_SYMBOL } from '~/lib/constants'
 
 const iconMap: {
   [key: string]: ReactElement;
@@ -28,16 +32,39 @@ export function WalletMenu({ closeModal }: { closeModal: Function }) {
   const { data: ensName } = useEnsName({ address })
   const { connect, connectors, error, isLoading, pendingConnector } = useConnect()
   const { disconnect } = useDisconnect()
+  const [tokenBalance, setTokenBalance] = useState<null | number>(null);
+
+  useEffect(() => {
+    if (isConnected) {
+      const fetchBalance = async () => {
+        // TODO: change to fetch Tulip balance
+        const balance = await getCurrentBlock();
+        setTokenBalance(Number(balance));
+      }
+      fetchBalance();
+    }
+  }, [isConnected]);
 
   if (isConnected) {
     return (
-      <div className='flex flex-col items-center justify-center p-5 w-[30rem] rounded-2xl bg-v3-bg'>
-        <div className='text-left font-semibold text-white/80 text-xl pb-8'>
+      <div className="flex flex-col items-center justify-center p-5 w-[30rem] rounded-2xl bg-v3-bg">
+        <div onClick={() => closeModal()} className="self-end text-white/50 hover:text-white">
+          <CloseIcon />
+        </div>
+        <div className="text-left font-semibold text-white/80 text-xl pb-4">
           Account: {ensName ? `${ensName} (${truncateAddress(address!)})` : truncateAddress(address!)}
+        </div>
+
+        <div className="text-left font-semibold text-xl pb-8">
+          Balance: <span className={
+            twMerge(
+              classNames("text-v3-primary", { tokenBalance: "animate-pulse" })
+            )
+          }>{tokenBalance ? tokenBalance : '...'}{TOKEN_SYMBOL}</span>
         </div>
         <button
           onClick={() => disconnect()}
-          className='block p-4 my-2 w-[15rem] border-gray-700 border-2 rounded-lg text-white/60 hover:text-white bg-slate-700/20'
+          className="block p-4 my-2 w-[15rem] border-gray-700 border-2 rounded-lg text-white/60 hover:text-white bg-slate-700/20"
         >
           Disconnect
         </button>
@@ -46,17 +73,17 @@ export function WalletMenu({ closeModal }: { closeModal: Function }) {
   }
 
   return (
-    <div className='p-5 w-[30rem] rounded-2xl bg-v3-bg'>
-      <div className='flex justify-between'>
-        <div className='text-left font-semibold text-white/80 text-xl pb-8'>
+    <div className="p-5 w-[30rem] rounded-2xl bg-v3-bg">
+      <div className="flex justify-between">
+        <div className="text-left font-semibold text-white/80 text-xl pb-8">
           Connect your wallet
-          <p className='text-sm text-white/50'>Select your wallet from these supported options</p>
+          <p className="text-sm text-white/50">Select your wallet from these supported options</p>
         </div>
-        <div onClick={() => closeModal()} className='text-white/50 hover:text-white'>
+        <div onClick={() => closeModal()} className="text-white/50 hover:text-white">
           <CloseIcon />
         </div>
       </div>
-      <div className='flex flex-col pb-8 items-center justify-stretch'>
+      <div className="flex flex-col pb-8 items-center justify-stretch">
         {connectors.map((connector) => (
           <button
             key={connector.id}
@@ -64,7 +91,7 @@ export function WalletMenu({ closeModal }: { closeModal: Function }) {
               connect({ connector });
               closeModal();
             }}
-            className='flex gap-2 p-4 my-2 w-[15rem] border-gray-700 border-2 rounded-lg text-white/60 hover:text-white bg-slate-600/20'
+            className="flex gap-2 p-4 my-2 w-[15rem] border-gray-700 border-2 rounded-lg text-white/60 hover:text-white bg-slate-600/20"
           >
             {iconMap[connector.name]}
             {connector.name}
@@ -74,7 +101,7 @@ export function WalletMenu({ closeModal }: { closeModal: Function }) {
           </button>
         ))}
       </div>
-      {error && <div className='text-red-400/90'>{error.message}</div>}
+      {error && <div className="text-red-400/90">{error.message}</div>}
     </div >
   )
 }
